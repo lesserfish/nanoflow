24c7b9ec09d34e20b6cb9d3f5e86d306348d9918
lesserfish <lesserfish@pm.me>
2023-06-14 19:36:20 -0300

    Now it works with the moon example

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -5,17 +5,29 @@ import System.Random
 import Text.Printf (printf)
 
 
-generateTrainingSet :: Int -> (Double, Double) -> Network -> IO [(Matrix Double, Matrix Double)]
-generateTrainingSet n (min, max) net 
-    | n <= 0 = return []
-    | otherwise = do
-        let input_size = inputSize net
-        linput <- runif input_size (min, max)
-        let input = fromList input_size 1 linput
-        let pred = prediction (feedforward linput net)
-        tail <- generateTrainingSet (n - 1) (min, max) net
-        let output = [(input, pred)] ++ tail
-        return $ output
+add_noise :: Double -> [Double] -> IO [Double]
+add_noise epsilon values = do
+    noise <- runif (length values) ((-epsilon), epsilon)
+    let output = zipWith (+) values noise
+    return output
+
+make_moon :: Int -> Double -> IO [(Matrix Double, Matrix Double)]
+make_moon n epsilon = do
+    let step_size = pi/(fromIntegral n) :: Double
+    let steps = [0, 0 + step_size .. pi]
+    let outer' = [[cos alpha, sin alpha] | alpha <- steps] :: [[Double]]
+    let inner' = [[1 - cos alpha, 0.5 - sin alpha] | alpha <- steps] :: [[Double]]
+    outer <- mapM (add_noise 0.1) outer'
+    inner <- mapM (add_noise 0.1) inner'
+    let outer_position = fmap (fromList 2 1) outer
+    let outer_class = replicate (length outer_position) (fromList 1 1 [0])
+    let inner_position = fmap (fromList 2 1) inner
+    let inner_class = replicate (length inner_position) (fromList 1 1 [1])
+    let outer_object = zip outer_position outer_class :: [(Matrix Double, Matrix Double)]
+    let inner_object = zip inner_position inner_class :: [(Matrix Double, Matrix Double)]
+    let output = outer_object ++ inner_object
+    return $ output
+
 
 overall_error :: Error -> [(Matrix Double, Matrix Double)] -> Network -> Double
 overall_error err [] net = 0
@@ -51,11 +63,24 @@ loop n rate net training_set
         output <- loop (n - 1) rate newnet training_set 
         return $ output
 
+evaluate :: Network -> [(Matrix Double, Matrix Double)] -> IO Double
+evaluate network [] = return 0
+evaluate network (y:ys) = do
+    let (input, output) = y :: (Matrix Double, Matrix Double)
+    let pred' = getElem 1 1 (prediction (feedforward (toList input) network)) :: Double
+    let pred = if pred' > 0.5 then 1 else 0
+--    putStrLn $ "Correct: " ++ (show . (getElem 1 1) $ output) ++ " Estimated: " ++ show pred
+    let hit = if pred == (getElem 1 1 output) then 1 else 0
+    rest <- evaluate network ys
+    return $ hit + rest
+
 main :: IO ()
 main = do
-  network <- inputLayer 1 >>= pushLayer 16 htan >>= pushLayer 16 htan >>= pushLayer 1 idd
-  training_set <- generateTrainingSet 10 (0.0, 1.0) network
+  training_set <- make_moon 100 0.01
 
-  model <- inputLayer 1 >>= pushLayer 16 htan >>= pushLayer 16 htan >>= pushLayer 1 idd
-  loop 300 (0.0001) model training_set
-  putStrLn "Over!"
+  model <- inputLayer 2 >>= pushLayer 6 htan >>= pushLayer 6 htan >>= pushLayer 1 htan
+  result <- loop 500 (0.001) model training_set
+  acc <- (evaluate result training_set)
+  let proportion = acc / (fromIntegral . length $ training_set) :: Double
+  putStrLn $ "Accuracy: " ++ show proportion
+  return ()
